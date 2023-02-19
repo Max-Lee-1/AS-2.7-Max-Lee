15290b3912744178ed445a95e479a4e481f33af6
Max Lee <m.m.lee@student.saintkentigern.com>
2023-02-20 10:01:22 +1300

    collection commit testing 2

--- collection.py
+++ collection.py
@@ -1,1 +1,15 @@
-# testing
+def collection_testing():
+    a_list = [1, 2, 3, 4]
+    a_tuple = (1, [2.0, 2.1, 2.2], 3, 4.0)
+    a_dictionary = {1: 'One', 2: 'Two', 3: 'Three'}
+    print(a_list)
+    print(type(a_list))
+    print(a_tuple)
+    print(type(a_tuple))
+    print(a_dictionary)
+    print(type(a_dictionary))
+
+
+if __name__ == '__main__':
+    collection_testing()
+

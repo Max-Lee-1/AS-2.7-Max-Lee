c1843780f9e39370898fe78eab49bab823a1c1c8
Max Lee <m.m.lee@student.saintkentigern.com>
2023-02-24 11:36:57 +1300

    collection task iteration 5 (adding array)

--- collection.py
+++ collection.py
@@ -1,49 +1,7 @@
+import array as arr
 
-'''def collection_testing(a):
-    a_list = [1, 2, 3, 4]
-    a_tuple = (1, [2.0, 2.1, 2.2], 3, 4.0)
-    a_set = {1, 'Two', -3, 4.0}
-    a_dictionary = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4}
-    # list
-    print('List: ', a_list, '\n',
-          type(a_list), '\n',
-          'First:', a_list[0], '; Last:', a_list[-1], '\n',
-          a_list[:int(len(a_list)/2)])
-    for i in a_list:
-        print(i.__index__(), ')', i)
-    for i in a_list:
-        i = int(input('Type: '))
-        if 1 <= i <= 4:
-            print(a_list[i-1])
-            break
-        else:
-            print('Invalid.')
-
-    print('\n')
-    # tuple
-    print('Tuple: ', a_tuple, '\n',
-          type(a_tuple), '\n',
-          'First:', a_tuple[0], '; Last: ', a_tuple[-1], '\n',
-          a_tuple[:int(len(a_tuple)/2)])
-    print('\n')
-    # set
-    print('Set: ', a_set, '\n',
-          type(a_set), '\n',
-          'First:', list(a_set)[0], '; Last:', list(a_set)[-1], '\n',
-          list(a_set)[:len(a_set)//2])
-    print('\n')
-    # dictionary
-    print('Dictionary: ', a_dictionary, '\n',
-          type(a_dictionary), '\n',
-          "First: ", list(a_dictionary.keys())[0], ',', list(a_dictionary.values())[0], '\n',
-          "Last: ", list(a_dictionary.keys())[-1], ',', list(a_dictionary.values())[-1], '\n',
-          list(a_dictionary.items())[:len(a_dictionary)//2])
-    # for (key, value) in a_dictionary.items():
-        # print ('%r: %r' % (key, value))
-    # print(a_dictionary[:int(len(a_dictionary)/2)]) # call first half
-    '''
-
-a_list = [1, 2, 3, 4]
+a_list = [1, 2, 3, "4"]
+a_array = arr.array("b", [1, 2, 3, 4])
 a_tuple = (1, [2.0, 2.1, 2.2], 3, 4.0)
 a_set = {1, 'Two', -3, 4.0}
 a_dictionary = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4}
@@ -73,6 +31,12 @@ def collection_testing(a):
                 print('Invalid input.')
 
 
+if __name__ == '__main__':
+    collection_testing(a_list)
+    collection_testing(a_array)
+    collection_testing(a_tuple)
+    collection_testing(a_set)
+    collection_testing(a_dictionary)
 
     '''if type(a) == list or type(a) == tuple: 
         
@@ -129,11 +93,48 @@ def collection_testing(a):
               "First: ", list(a_dictionary.keys())[0], ',', list(a_dictionary.values())[0], '\n',
               "Last: ", list(a_dictionary.keys())[-1], ',', list(a_dictionary.values())[-1], '\n',
               list(a_dictionary.items())[:len(a_dictionary)//2])
-        '''
-
-
-if __name__ == '__main__':
-    collection_testing(a_list)
-    collection_testing(a_tuple)
-    collection_testing(a_set)
-    collection_testing(a_dictionary)
+        
+        
+    def collection_testing(a):
+        a_list = [1, 2, 3, 4]
+        a_tuple = (1, [2.0, 2.1, 2.2], 3, 4.0)
+        a_set = {1, 'Two', -3, 4.0}
+        a_dictionary = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4}
+        # list
+        print('List: ', a_list, '\n',
+              type(a_list), '\n',
+              'First:', a_list[0], '; Last:', a_list[-1], '\n',
+              a_list[:int(len(a_list)/2)])
+        for i in a_list:
+            print(i.__index__(), ')', i)
+        for i in a_list:
+            i = int(input('Type: '))
+            if 1 <= i <= 4:
+                print(a_list[i-1])
+                break
+            else:
+                print('Invalid.')
+    
+        print('\n')
+        # tuple
+        print('Tuple: ', a_tuple, '\n',
+              type(a_tuple), '\n',
+              'First:', a_tuple[0], '; Last: ', a_tuple[-1], '\n',
+              a_tuple[:int(len(a_tuple)/2)])
+        print('\n')
+        # set
+        print('Set: ', a_set, '\n',
+              type(a_set), '\n',
+              'First:', list(a_set)[0], '; Last:', list(a_set)[-1], '\n',
+              list(a_set)[:len(a_set)//2])
+        print('\n')
+        # dictionary
+        print('Dictionary: ', a_dictionary, '\n',
+              type(a_dictionary), '\n',
+              "First: ", list(a_dictionary.keys())[0], ',', list(a_dictionary.values())[0], '\n',
+              "Last: ", list(a_dictionary.keys())[-1], ',', list(a_dictionary.values())[-1], '\n',
+              list(a_dictionary.items())[:len(a_dictionary)//2])
+        # for (key, value) in a_dictionary.items():
+            # print ('%r: %r' % (key, value))
+        # print(a_dictionary[:int(len(a_dictionary)/2)]) # call first half
+    '''

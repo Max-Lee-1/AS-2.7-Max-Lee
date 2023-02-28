ab591801482f662d2fab2ae07757f908ffa0cb0e
Max Lee <m.m.lee@student.saintkentigern.com>
2023-03-01 09:52:54 +1300

    commenting

---
+++ AS 2.7 Headphone Recommendation.py
@@ -0,0 +1,15 @@
+''' Headphone/Earbuds recommendation program
+    users have to fill in requirements to narrow result
+    Two categories of True Wireless(TW): TW Headphones & TW Earbuds (use collections)
+    Each category has 10-20 in collections, total less than 40
+    Requirements (variables): Price range: $1 - $2000
+                              Brand: [insert selection chart]
+                              Functions: E.g. Noise Cancelling, Ambient sound...
+                              Support Codec: E.g. AAC, SBC, LDAC...
+                              App support: Y/N / Both
+                              Battery Life: [Selection time range]
+                              Bluetooth Ver. (added from reference): 5.0 or above, 5.2 or above...
+                              Rating (added form reference): the best overall, Best in audio, best in user-friendly...
+'''
+
+

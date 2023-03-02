e6837c275f058132011fcaadc315de97e4651972
Max Lee <m.m.lee@student.saintkentigern.com>
2023-03-03 11:38:34 +1300

    dictionary set up 1.1

--- AS 2.7 Headphone Recommendation.py
+++ AS 2.7 Headphone Recommendation.py
@@ -9,36 +9,41 @@
                               App support: Y/N / Both
                               Battery Life: [Selection time range]
                               Bluetooth Ver. (added from reference): 5.0 or above, 5.2 or above...
-                              Rating (added form reference): the best overall, Best in audio, best in user-friendly...
+                              Speciality (added form reference): the best overall, Best in audio, best in user-friendly...
+                              Info: Ship Y/N; Retail Y/N; Overall rating (May add other ratings);
 '''
-headphone_dict = {'Sony WH-1000XM5': 1,  # best all rounded, best ANC, comfortable
-                  'Apple Airpods Max': 2,  # best iphone, most expensive, 2nd best for workout
-                  'Sony WH-1000XM4': 3,  # best sounding bass, best for workout, Best Bluetooth codec support
-                  'Bose Noise Cancelling Headphones 700': 4,  # best phone call
-                  'Focal Bathys': 6,  # best sounding (over-ear)
-                  'Logitech G535 LIGHTSPEED Wireless': 7,  # best budget gaming
-                  'Bowers & Wilkins Px7 S2 Headphones': 8,  # best look, most comfort, 3rd best Bluetooth codec support
-                  'Sennheiser Momentum 4 Wireless': 9,  # 2nd best sounding bass, 2nd best Bluetooth codec support
-                  'Belkin Soundform Mini': 10,  # best for kids
-                  'Audio-Technica ATH-M20xBT': 11,  # best cheapest
-                  'Anker SoundCore Life Q30': 12,  # best value
-                  'Edifier W820NB': 13,  # best sounding cheapest
+headphone_dict = {'Sony WH-1000XM5': 595.00,  # best all rounded, best ANC, comfortable
+                  'Apple Airpods Max': 999.00,  # best iphone, 2nd most expensive, 2nd best for workout
+                  'Sony WH-1000XM4': 414.00,  # best sounding bass, best for workout, Best Bluetooth codec support
+                  'Bose Noise Cancelling Headphones 700': 699.95,  # best phone call
+                  'Logitech G435 LIGHTSPEED Wireless': 249.90,  # best budget gaming
+                  'Bowers & Wilkins Px7 S2 Headphones': 649.00,  # best look, most comfort, 3rd best Bluetooth codec support
+                  'Sennheiser Momentum 4 Wireless': 599.95,  # 2nd best sounding bass, 2nd best Bluetooth codec support
+                  'Belkin Soundform Mini': 59.99,  # best for kids
+                  'Anker SoundCore Life Q30': 241.95,  # best value
+                  'Edifier W820NB': 168.00,  # best sounding cheapest
                   }
 
-earbud_dict = {'Samsung Galaxy Buds 2': 1,  # best all rounded
-               'Apple Airpods Pro (Gen 2)': 2,  # best iphone, best sounding upper-mid range price, most expensive
-               'Beats Fit Pro': 3,  # best workout
-               'Sony WF-1000XM4': 4,  # best ANC (Active Noise Cancelling), 2nd best Bluetooth codec support
-               'Jabra Elite 7 Active': 5,  # best versatility
-               'Jabra Elite 7 Pro': 6,  # best sounding in lower-mid range price
-               'EPOS GTW 270 Hybrid': 7,  # best gaming
-               'Nothing Ear 1': 8,  # best looking
-               'Sennheiser Momentum TW 3': 9,  # best sounding bass
-               'Earfun Air Pro': 10,  # good-looking, all rounded in cheap price range
+earbud_dict = {'Samsung Galaxy Buds 2': 279.00,  # best all rounded
+               'Apple Airpods Pro (Gen 2)': 479.00,  # best iphone, best sounding upper-mid range price, most expensive
+               'Beats Fit Pro': 349.95,  # best workout
+               'Sony WF-1000XM4': 369.00,  # best ANC (Active Noise Cancelling), 2nd best Bluetooth codec support
+               'Jabra Elite 7 Active': 284.00,  # best versatility
+               'Jabra Elite 7 Pro': 296.00,  # best sounding in lower-mid range price
+               'EPOS GTW 270 Hybrid': 239.00,  # best gaming
+               'Nothing Ear 1': 189.00,  # best looking
+               'Sennheiser Momentum TW 3': 378.00,  # best sounding bass
+               'Earfun Air Pro 3': 139.00,  # good-looking, all rounded in cheap price range
                '1MORE PistonbBuds Pro': 11,  # best cheapest with all rounded feature
                'Bang & Olufsen Beoplay EX': 12,  # Expensive, great-looking, nice sound
                'Jabra Elite 85t': 13,  # best Bluetooth codec support
                'OnePlus Nord Buds': 14,  # best value
                'Sony WF-C500': 15,  # best sounding cheapest
                 }
+
+# headphone_dict[1] =
+
+print(headphone_dict)
+print('\n')
+print(earbud_dict)
 

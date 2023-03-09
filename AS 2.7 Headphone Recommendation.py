d03195e93b5ecb46dd3e2a54b8d9b5e1ba84732c
Max Lee <m.m.lee@student.saintkentigern.com>
2023-03-09 15:30:09 +1300

    Variable and function set-up

--- AS 2.7 Headphone Recommendation.py
+++ AS 2.7 Headphone Recommendation.py
@@ -38,7 +38,42 @@ earbud_dict = {'Samsung Galaxy Buds 2': 279.00,  # best all rounded
                'Sony WF-C500': 115.00,  # best sounding cheapest
                }
 
-'''# Headphone/Earbuds Recommendation Program
+'''
+09-03:
+# Headphone/Earbuds Recommendation Program
+
+headphone_dict = {'Sony WH-1000XM5': {'price': 595.00, 'speciality': 'best all rounded, best ANC, comfortable'},  
+                  'Apple Airpods Max': {'price': 999.00, 'speciality': 'best iphone, 2nd most expensive, 2nd best for workout'},  
+                  'Sony WH-1000XM4': {'price': 414.00, 'speciality': 'best sounding bass, best for workout, Best Bluetooth codec support'},  
+                  'Bose Noise Cancelling Headphones 700': {'price': 699.95, 'speciality': 'best phone call'},  
+                  'Logitech G435 LIGHTSPEED Wireless': {'price': 249.90, 'speciality': 'best budget gaming'},  
+                  'Bowers & Wilkins Px7 S2 Headphones': {'price': 649.00, 'speciality': 'best look, most comfort, 3rd best Bluetooth codec support'},  
+                  'Sennheiser Momentum 4 Wireless': {'price': 599.95, 'speciality': '2nd best sounding bass, 2nd best Bluetooth codec support'},  
+                  'Belkin Soundform Mini': {'price': 59.99, 'speciality': 'best for kids'},  
+                  'Anker SoundCore Life Q30': {'price': 241.95, 'speciality': 'best value'},  
+                  'Edifier W820NB': {'price': 168.00, 'speciality': 'best sounding cheapest'},  
+                  }
+
+earbud_dict = {'Samsung Galaxy Buds 2': {'price': 279.00, 'speciality': 'best all rounded'},  
+               'Apple Airpods Pro (Gen 2)': {'price': 479.00, 'speciality': 'best iphone, best sounding upper-mid range price, expensive'},  
+               'Beats Fit Pro': {'price': 349.95, 'speciality': 'best workout'},  
+               'Sony WF-1000XM4': {'price': 369.00, 'speciality': 'best ANC (Active Noise Cancelling), 2nd best Bluetooth codec support'},  
+               'Jabra Elite 7 Active': {'price': 284.00, 'speciality': 'best versatility'},  
+               'Jabra Elite 7 Pro': {'price': 296.00, 'speciality': 'best sounding in lower-mid range price'},  
+               'EPOS GTW 270 Hybrid': {'price': 239.00, 'speciality': 'best gaming'},  
+               'Nothing Ear 1': {'price': 189.00, 'speciality': 'best looking'},  
+               'Sennheiser Momentum TW 3': {'price': 378.00, 'speciality': 'best sounding bass'},  
+               'Earfun Air Pro 3': {'price': 139.00, 'speciality': 'best value, good-looking, all rounded'},  
+               'Bang & Olufsen Beoplay EX': {'price': 775.00, 'speciality': 'Most expensive, great-looking, nice sound'},  
+               'Jabra Elite 85t': {'price': 199.95, 'speciality': 'best Bluetooth codec support'},  
+               'Sony WF-C500': {'price': 115.00, 'speciality': 'best sounding cheapest'},  
+                }
+
+# Price range
+price_range = input("Enter price range (e.g. 100-500): ").split("-")
+
+
+# Headphone/Earbuds Recommendation Program
 
 headphone_dict = {'Sony WH-1000XM5': 595.00,  
                   'Apple Airpods Max': 999.00,  
@@ -117,6 +152,79 @@ bluetooth = input("Enter Bluetooth version (" + ", ".join(bluetooth
                             recommendations.append(product)
 
     return recommendations
+    
+    # Headphone/Earbuds Recommendation Program
+
+headphone_dict = {
+    'Sony WH-1000XM5': 595.00,  
+    'Apple Airpods Max': 999.00,  
+    'Sony WH-1000XM4': 414.00,  
+    'Bose Noise Cancelling Headphones 700': 699.95,  
+    'Logitech G435 LIGHTSPEED Wireless': 249.90,  
+    'Bowers & Wilkins Px7 S2 Headphones': 649.00,  
+    'Sennheiser Momentum 4 Wireless': 599.95,  
+    'Belkin Soundform Mini': 59.99,  
+    'Anker SoundCore Life Q30': 241.95,  
+    'Edifier W820NB': 168.00  
+}
+
+earbud_dict = {
+    'Samsung Galaxy Buds 2': 279.00,  
+    'Apple Airpods Pro (Gen 2)': 479.00,  
+    'Beats Fit Pro': 349.95,  
+    'Sony WF-1000XM4': 369.00,  
+    'Jabra Elite 7 Active': 284.00,  
+    'Jabra Elite 7 Pro': 296.00,  
+    'EPOS GTW 270 Hybrid': 239.00,  
+    'Nothing Ear 1': 189.00,  
+    'Sennheiser Momentum TW 3': 378.00,  
+    'Earfun Air Pro 3': 139.00,  
+    'Bang & Olufsen Beoplay EX': 775.00,  
+    'Jabra Elite 85t': 199.95,  
+    'Sony WF-C500': 115.00  
+}
+
+# Price range
+price_range = input("Enter price range (e.g. 100-500): ").split("-")
+price_min = float(price_range[0])
+price_max = float(price_range[1])
+
+# Brand
+brand_choices = ["Sony", "Apple", "Bose", "Logitech", "Bowers & Wilkins", "Sennheiser"]
+print("Choose a brand from the following: ")
+for i, brand in enumerate(brand_choices):
+    print(f"{i+1}. {brand}")
+brand_choice = int(input("Enter your choice: "))
+brand = brand_choices[brand_choice-1]
+
+# Functions
+functions_choices = ["Noise Cancelling", "Ambient sound"]
+print("Select functions you want from the following: ")
+for i, function in enumerate(functions_choices):
+    print(f"{i+1}. {function}")
+functions_choice = int(input("Enter your choice: "))
+functions = functions_choices[functions_choice-1]
+
+# Codec support
+codec_choices = ["AAC", "SBC", "LDAC"]
+print("Select codec support you want from the following: ")
+for i, codec in enumerate(codec_choices):
+    print(f"{i+1}. {codec}")
+codec_choice = int(input("Enter your choice: "))
+codec = codec_choices[codec_choice-1]
+
+# App support
+app_choices = ["Y", "N", "Both"]
+print("Select app support you want from the following: ")
+for i, app in enumerate(app_choices):
+    print(f"{i+1}. {app}")
+app_choice = int(input("Enter your choice: "))
+app = app_choices[app_choice-1]
+
+# Battery life
+battery_life_choices = ["<5 hours", "5-10 hours", "10-20 hours", ">20 hours"]
+print("Select battery life you want from the following: ")
+
 '''
 
 price_min = 0
@@ -126,6 +234,7 @@ codec = "N/A"
 app_support = "N/A"
 battery = "N/A"
 speciality = "N/A"
+
 
 # Price range
 def price_range_fc():
@@ -140,7 +249,11 @@ def price_range_fc():
 def brand_fc():
     brand_choices = ["Anker", "Apple", "Bang & Olufsen", "Beats", "Belkin", "Bose", "Bowers & Wilkins", "Edifier", "EPOS",
                      "Jabra", "Logitech", "Nothing", "Samsung", "Sennheiser", "Sony", "N/A"]
-    return brand == input("Enter brand (" + ", ".join(brand_choices) + "): ")
+    print("Choose a brand from the following: ")
+    for i, brand in enumerate(brand_choices):
+        print(f"{i+1}. {brand}")
+    brand_choice = int(input("Enter your choice: "))
+    return brand == brand_choices[brand_choice-1]
 
 
 # Functions
@@ -153,30 +266,34 @@ def functions_fc():
 
 # Support Codec
 def codec_fc():
+    global codec
     codec_choices = ["AAC", "aptX", "aptX Low Latency", "aptX Adaptive", "aptX HD", "LC3", "LDAC", "LHDC", "SBC", "N/A"]
     return codec == input("Enter support codec (" + ", ".join(codec_choices) + "): ")
 
 
 # App support
 def app_support_fc():
+    global app_support
     app_support_choices = ["Yes", "No", "N/A"]
     return app_support == input("Enter app support (" + ", ".join(app_support_choices) + "): ")
 
 
 # Battery Life
 def battery_life_fc():
+    global battery
     battery_choices = ["Less than 5 hours", "5-10 hours", "10-20 hours", "More than 20 hours", "N/A"]
     return battery == input("Enter battery life (" + ", ".join(battery_choices) + "): ")
 
 
 # Speciality
 def speciality_fc():
+    global speciality
     speciality_choices = ["Value", "Cheapest", "Pricey", "Long Usage Hours", "Good Sounding (in price range)",
                           "Best ANC", "Best Ambient", "Best Overall"]
     return speciality == input("Enter speciality (" + ", ".join(speciality_choices) + "): ")
 
 
-def recommendation():
+def recommendation(price_min, price_max, brand, functions, codec):
     # Initialize list of recommendations
     recommendations = []
 
@@ -198,7 +315,11 @@ def recommendation():
     return recommendations
 
 
-print(headphone_dict)
-print('\n')
-print(earbud_dict)
-
+price_range_fc()
+brand_fc()
+functions_fc()
+codec_fc()
+app_support_fc()
+battery_life_fc()
+speciality_fc()
+recommendation(price_min, price_max, brand, functions, codec)
